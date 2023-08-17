6f89bc10d29b0293d06451ba27fee0211a669abd
iamyahia <yahiabaiz@gmail.com>
2023-08-17 13:44:08 +0300

    refactor: change mock_data source and config the changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
 // import React from "react";
 import ProductCard from "./components/productCard";
+import { users } from "./mock_data.js";
 
 export default function App() {
-  return <ProductCard />;
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 1fr)",
+        gridColumnGap: "12px",
+        gridRowGap: "12px",
+      }}
+    >
+      {users.map((user, index) => (
+        <ProductCard
+          name={user.name}
+          date={user.date}
+          gender={user.gender}
+          email={user.email}
+          key={index}
+        />
+      ))}
+    </div>
+  );
 }
--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -1,34 +1,24 @@
 // import React from "react";
-import { users } from "./mock_data.js";
+import { users } from "../../mock_data.js";
 
-export default function ProductCard() {
+export default function ProductCard({ name, date, gender, email }) {
   console.log("users: ", users);
   return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(3, 1fr)",
-        gridColumnGap: "12px",
-        gridRowGap: "12px",
-      }}
-    >
-      {users.map((user, index) => (
-        <div
-          style={{
-            backgroundColor: "#d0f5f7",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-          key={index}
-        >
-          <p>name: {user.name}</p>
-          {user.date && <p>date: {user.date}</p>}
-          <p>gender: {user.gender}</p>
-          {user.email && <p>email: {user.email}</p>}
-        </div>
-      ))}
+    <div>
+      <div
+        style={{
+          backgroundColor: "#d0f5f7",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <p>name: {name}</p>
+        {date && <p>date: {date}</p>}
+        <p>gender: {gender}</p>
+        {email && <p>email: {email}</p>}
+      </div>
     </div>
   );
 }
